a185101045dc1b62b70d8966391031acd3af7c10
Hevilio <Hevilio@users.noreply.github.com>
2018-09-07 01:30:03 +0200

    Initial commit

---
+++ public/js/index.js
@@ -0,0 +1,129 @@
+vueshow = new Vue({
+	el : "#vueshow",
+	data : {
+		fics : [],
+		numberOfPosts : 0
+	},
+	methods : {
+		getposts : async function(){
+			let posts = await axios.get('/posts');
+			this.fics = posts.data;
+			this.numberOfPosts = posts.length;
+			console.log(posts.data);
+		}
+	}
+});
+
+vuepost = new Vue({
+	el : "#vuepost",
+	data: {
+		empty : false,
+		logged : false
+	},
+	methods : {
+		postfic : async function(){
+			try{
+				let fic = {
+					title : this.$refs.title.value,
+					content : this.$refs.text.value,
+					author : vuelog.username,
+				};
+				console.log(fic);
+				if (fic.title != '' && fic.content != '')
+				{
+					this.empty =  false;
+					let response = await axios({
+						method: 'post',
+						url : '/postafic',
+						data : fic
+					})
+					vueshow.getposts();
+				}
+				else
+					this.empty = true;
+			}catch(error){
+				console.log(error);
+			}
+		}
+	}
+});
+
+vuelog = new Vue({
+	el : '#vuelog',
+	data : {
+		selected : 'Log In',
+		logged : false,
+		empty : false,
+		username : '',
+		error : '',
+	},
+	methods : {
+		register:async function(){
+			let user = {
+				username : this.$refs.username.value,
+				password : this.$refs.password.value
+			};
+			if (user.username != '' && user.password != '')
+			{
+				this.empty = false;
+				let response = await axios({
+					method: 'post',
+					url : '/register',
+					data : user
+				});
+				if (response.data == "Success")
+				{
+					vuepost.logged = true;
+					this.logged = true,
+					this.username = user.username;
+					this.error = '';
+				}
+				else
+					this.error = response.data;
+			}
+			else
+				this.empty = true;
+		},
+		login:async function(){
+			let user = {
+				username : this.$refs.username.value,
+				password : this.$refs.password.value
+			};
+			if (user.username != '' && user.password != ''){
+				this.empty = false;
+				let response = await axios({
+					method: 'post',
+					url : '/login',
+					data : user
+				});
+				if (response.data == "Success")
+				{
+					vuepost.logged = true;
+					this.logged = true,
+					this.username = user.username;
+					this.error = '';
+				}
+				else
+					this.error = response.data;
+			}
+			else
+				this.empty = true;
+		},
+		submit:async function(){
+			if (this.selected == "Log In")
+			{
+				this.login();
+			}
+			else
+				this.register();
+		},
+		logout: async function(){
+			this.username = '';
+			this.logged = false;
+			this.empty = false;
+			this.error = '';
+			vuepost.logged = false;
+		}
+	}
+})
+vueshow.getposts();
---
+++ server.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
+var port = 3000;
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/views/index.html');
+});
+
+var posts = [];
+var users = [];
+
+app.get('/posts', (req, res) => {
+	res.send(posts);
+	});
+;
+app.post('/postafic', (req,res) => {
+		let today = new Date();
+		let date = today.getDate() + '/' + today.getMonth() + '/' + today.getFullYear();
+		posts.unshift({
+			title: req.body.title,
+			content: req.body.content,
+			author: req.body.author,
+			date: date,
+		})
+		res.send(posts);
+})
+
+app.post('/register', (req,res) => {
+	let message = '';
+	let data = req.body;
+	users.forEach((user) => {
+		if (data.username == user.username)
+			message = "Eusername";
+	});
+	users.push({
+		username : data.username,
+		password : data.password
+	})
+	res.send(message == '' ? "Success" : message);
+});
+
+app.post('/login', (req,res) => {
+	let data = req.body;
+	let message = '';
+	users.forEach((user) => {
+		if (data.username == user.username && data.password == user.password)
+			message = "Success";
+		if (data.username == user.username && data.password != user.password)
+			message = "Wpassword";
+	});
+	res.send(message == ''? "Wusername" : message);
+});
+
+console.log('Server is listening on port ' + port);
+app.listen(port);
